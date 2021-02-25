7279911d49c7e4ac865735596a956cc5daba5a27
Shawn <shawntaylor@weecology.org>
2021-02-25 12:02:12 -0700

    add large fig of series counts per agency

--- exploratory.R
+++ exploratory.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+library(patchwork)
 
 
 filter_stuff = function(df){
@@ -18,7 +18,9 @@ filter_stuff = function(df){
 fed_data = read_csv('./data/processed_data.csv.gz') %>%
   filter_stuff()
 
-# Bar Plot of MS/PhD positions in GS-9/10/11/12 for 09-20
+#-------------------------------
+# Bar Plot of MS/PhD positions in GS-9/10/11/12 for 18-20
+#-------------------------------
 fed_data %>%
   filter(!is.na(perm_status)) %>%
   filter(!agency %in% c('Military','VA','NSF')) %>%
@@ -42,14 +44,16 @@ fed_data %>%
         axis.title.x = element_blank(),
         strip.background = element_blank(),
         strip.text       = element_text(hjust=0)) +
-  labs(x='', y='Average Number of Employees 2008-2020', fill='Permananent\nStatus',
+  labs(x='', y='Average Number of Employees 2018-2020', fill='Permananent\nStatus',
        subtitle = 'GS 9/11/12 Physical & Natural Science employees with an MS or PhD',
        caption='')
 
 
 
 
+#-------------------------------
 # MS/PhD positions over time in GS-9/10/11/12
+#-------------------------------
 fed_data %>%
   filter(!is.na(perm_status)) %>%
   filter(!agency %in% c('Military','VA','NSF')) %>%
@@ -75,3 +79,81 @@ fed_data %>%
        caption='*Does not account for promotions beyond G12 or self-reporting of education level') +
   guides(linetype = guide_legend('Permananent Status', override.aes = list(size=2), keywidth = unit(40,'mm')),
          color    = guide_legend(title=element_blank(),override.aes = list(size=2)))
+
+
+#-------------------------------
+# Large plot with specific occupations per agency
+#-------------------------------
+compress_some_occupations = function(df){
+  low_count_occupations = c('1372-GEODESY','0405-PHARMACOLOGY','1384-TEXTILE TECHNOLOGY',
+                            '0459-IRRIGATION SYSTEM OPERATION','1321-METALLURGY')
+  df %>%
+    mutate(occupation_desc = case_when(
+      occupation_desc == '1371-CARTOGRAPHIC TECHNICIAN' ~ '1370-CARTOGRAPHY',
+      occupation_desc == '1316-HYDROLOGIC TECHNICIAN'   ~ '1315-HYDROLOGY',
+      occupation_desc == '1399-PHYSICAL SCIENCE STUDENT TRAINEE' ~ '1301-GENERAL PHYSICAL SCIENCE',
+      occupation_desc == '1311-PHYSICAL SCIENCE TECHNICIAN' ~ '1301-GENERAL PHYSICAL SCIENCE',
+      occupation_desc == '0401-GENERAL NATURAL RESOURCES MANAGEMENT AND BIOLOGICAL SCIENCES' ~ '0401-GENERAL BIOLOGY/NATURAL RESOURCES',
+      occupation_desc == '0404-BIOLOGICAL SCIENCE TECHNICIAN' ~ '0401-GENERAL BIOLOGY/NATURAL RESOURCES', # 404/401 are pretty much the same
+      occupation_desc == '0499-BIOLOGICAL SCIENCE STUDENT TRAINEE' ~ '0401-GENERAL BIOLOGY/NATURAL RESOURCES',
+      occupation_desc == '0455-RANGE TECHNICIAN' ~ '0454-RANGELANDS',
+      occupation_desc == '0454-RANGELAND MANAGEMENT' ~ '0454-RANGELANDS',
+      occupation_desc == '1316-HYDROLOGIC TECHNICIAN' ~ '1315-HYDROLOGY',
+      occupation_desc == '1341-METEOROLOGICAL TECHNICIAN' ~ '1340-METEOROLOGY',
+      occupation_desc == '0480-FISH AND WILDLIFE ADMINISTRATION' ~ '0480-FISH/WILDLIFE MGMT',
+      occupation_desc == '0485-WILDLIFE REFUGE MANAGEMENT' ~ '0480-FISH/WILDLIFE MGMT',
+      occupation_desc == '0458-SOIL CONSERVATION TECHNICIAN' ~ '0457-SOIL CONSERVATION',
+      occupation_desc == '0462-FORESTRY TECHNICIAN' ~ '0460-FORESTRY',
+      occupation_desc %in% low_count_occupations   ~ 'Other',
+      TRUE ~ occupation_desc
+    ))
+}
+
+occupation_agency_totals = fed_data %>%
+  filter(!is.na(perm_status)) %>%
+  filter(!agency %in% c('Military','VA','NSF')) %>%
+  filter(education %in% c('Masters','PhD')) %>%
+  filter(pay_plan  %in% c('GS-09','GS-11','GS-12')) %>%
+  #filter(agency %in% c('APHIS','ARS','DOE','NASA','NIH','USGS')) %>%
+  compress_some_occupations() %>%
+  filter(year %in% 2018:2020) %>%
+  count(year, agency, occupation_desc,perm_status) %>% 
+  group_by(agency, occupation_desc,perm_status) %>%
+  summarise(n = mean(n)) %>%
+  ungroup() %>%
+  #complete(agency,occupation_desc,perm_status, fill=list(n=0)) %>%
+  as_tibble()
+
+
+build_occupation_figure = function(agency_list, legend_position, title=NA, x_axis){
+  
+  occupation_agency_totals %>%
+    filter(agency %in% agency_list) %>%
+  ggplot(aes(y=str_wrap(occupation_desc,25), x=n, fill=perm_status)) +
+  geom_col() +
+  scale_fill_manual(values=c('#009e73','grey30')) +
+  facet_grid(~agency, scales='free') +
+  theme_bw() +
+  theme(legend.position =  legend_position,
+        legend.direction = 'horizontal',
+        axis.text.x =  element_text(size=10, color='black'),
+        axis.text.y = element_text(size=8, color='black'),
+        strip.background = element_blank(),
+        strip.text       = element_text(hjust=0, size=15)) +
+  labs(x=x_axis, y='', fill='Permananent Status',
+       subtitle = title,
+       caption='')
+}
+
+agencies = sample(unique(occupation_agency_totals$agency))
+top = build_occupation_figure(agencies[1:6], legend_position = 'none', title = 'GS 9/11/12 Physical & Natural Science employees with an MS or PhD',
+                              x_axis = '') +
+  theme(plot.subtitle = element_text(size=25))
+middle = build_occupation_figure(agencies[7:12], legend_position = 'none', title = '',
+                              x_axis = '') 
+bottom = build_occupation_figure(agencies[13:20], legend_position = 'bottom', title = '',
+                        x_axis = 'Average Number of Employees 2018-2020') +
+  theme(axis.title.x = element_text(size=20))
+
+top + middle + bottom + plot_layout(ncol=1)
+
