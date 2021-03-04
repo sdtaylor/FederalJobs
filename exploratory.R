eaeaaf4f2af645ea10599bcfe2aa09cd11a7d2d6
Shawn <shawntaylor@weecology.org>
2021-03-04 15:15:32 -0700

    drop timeseries plot

--- exploratory.R
+++ exploratory.R
@@ -71,36 +71,6 @@ ggsave('./summary_agency_count_figure.png', plot=summary_fig, width=28, height=4
 
 
 #-------------------------------
-# MS/PhD positions over time in GS-9/10/11/12
-#-------------------------------
-fed_data %>%
-  filter(!is.na(perm_status)) %>%
-  filter(!agency %in% c('Military','VA','NSF')) %>%
-  #filter(education %in% c('Masters','PhD')) %>%
-  filter(pay_plan  %in% c('GS-09','GS-10','GS-11','GS-12')) %>%
-  count(year, agency, pay_plan, perm_status) %>%
-  ggplot(aes(x=year, y=n, color=pay_plan)) +
-  geom_line(aes(linetype=perm_status)) + 
-  geom_point() + 
-  #scale_color_manual(values=c('black','#009e73')) + 
-  scale_color_brewer(palette = 'Dark2') +
-  scale_x_continuous(breaks=seq(2008,2020,2)) + 
-  facet_wrap(~agency, scales='free_y') +
-  theme_bw(15) +
-  theme(legend.position = c(0.7,0.1),
-        legend.direction = 'vertical',
-        legend.box       = 'horizontal',
-        axis.text.x =  element_text(size=10, angle=45, hjust=1),
-        strip.background = element_blank(),
-        strip.text       = element_text(hjust=0)) +
-  labs(x='', y='Number of employees',
-       subtitle = 'Physical & Natural Science Positions with an MS or PhD and at GS 9-12',
-       caption='*Does not account for promotions beyond G12 or self-reporting of education level') +
-  guides(linetype = guide_legend('Permananent Status', override.aes = list(size=2), keywidth = unit(40,'mm')),
-         color    = guide_legend(title=element_blank(),override.aes = list(size=2)))
-
-
-#-------------------------------
 # Large plot with specific occupations per agency
 #-------------------------------
 compress_some_occupations = function(df){
